2b5e1116facaad5b8db36bad73fb530562e7c326
Modiji238 <nishitsmodi238@gmail.com>
2025-03-16 03:23:56 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Modiji',
+    author_email= 'nishitsmodi238@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
